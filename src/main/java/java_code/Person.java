7dd7111679d6662c546b925629b3f08d926543b3
yusufk05 <ykaracin@gmail.com>
2019-12-26 16:44:25 -0500

    first commit

---
+++ src/main/java/java_code/Person.java
@@ -0,0 +1,9 @@
+package java_code;
+
+public class Person {
+    private int age;
+
+    public Person(int age){
+        this.age = age;
+    }
+}
---
+++ src/main/java/java_code/Student.java
@@ -0,0 +1,7 @@
+package java_code;
+
+public class Student extends Person {
+    public Student(int age){
+        super(age);
+    }
+}
